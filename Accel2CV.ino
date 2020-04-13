8b852b4aed6340fafe0c82e5039cd53517b27590
hana <hana.1995@mac.com>
2020-04-13 00:27:35 -0400

    Add comments

--- Accel2CV.ino
+++ Accel2CV.ino
@@ -2,13 +2,18 @@
 #include <ADXL345.h>
 
 ADXL345 accel(ADXL345_STD);
-constexpr byte CV_PIN = 9;
+constexpr byte CV_PIN = 9;  //CV出力ピン
 
-constexpr uint8_t CV_Max = 96;
-constexpr uint8_t CV_Min = 32;
-constexpr uint8_t CV_Range = CV_Max - CV_Min;
+constexpr uint8_t CV_Max = 96;  //CV電圧の最大値。
+constexpr uint8_t CV_Min = 32;  //CV電圧の最小値。
+constexpr uint8_t CV_Range = CV_Max - CV_Min; //計算用
 
 void setup() {
+  /* LPFをつけない場合はこの以下の２行のコメントアウトを解除してください。
+    TCCR1B &= B11111000;
+    TCCR1B |= B00000001;
+  */
+
   Wire.begin();
   accel.writeRange(ADXL345_RANGE_16G);
   accel.start();
@@ -17,12 +22,12 @@ void setup() {
 }
 
 void loop() {
-  if(accel.update()) {
+  if (accel.update()) {
     val = accel.getZ();
   }
 
   static float vol;
   vol = (val + 1.0) * 0.5 * CV_Range + CV_Min;
-    
+
   analogWrite(CV_PIN, static_cast<int>(vol));
 }
